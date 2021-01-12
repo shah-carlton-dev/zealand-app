5dc2709bbe299b4f360e73a7e400de8ddee40e05
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2021-01-12 12:04:43 -0500

    landing page work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,18 @@
 import React from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
+import { Container, Button, Row, Col, Jumbotron } from "react-bootstrap";
 
+
+// page components 
 import About from './About.jsx';
 import FAQ from './FAQ.jsx';
 import Hotel from './Hotel.jsx';
 import Welcome from './Welcome.jsx';
 import Contact from './Contact.jsx';
 
-
 const Home = () => {
 
     return (
         <>
-            <Container>
-                <Row>
-                    <h2>
-                        Home Section
-                    </h2>
-                </Row>
-            </Container>
-
             <Welcome></Welcome>
             <About></About>
             <Hotel></Hotel>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,15 +1,46 @@
 import React from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
-
+import { Container, Button, Row, Col, Jumbotron } from "react-bootstrap";
+import '../styles/Welcome.css';
+import { Link } from 'react-router-dom';
+import { HashLink as HLink } from 'react-router-hash-link';
+import logo from '../assets/backdrop-1.gif';
 const Welcome = () => {
 
     return (
         <>
-            <Container>
-                <Row>
-                    <h2> Welcome Section </h2>
-                </Row>
-            </Container>
+        <Container fluid className="bg-image" id="introsection">
+            <Row>
+                <Jumbotron className="bg-container intro-jumbotron bg">
+                    <Row className="m-0 p-0 jumbotronText center">                     
+                        <Col className='m-auto text-center d-none d-lg-block'>
+                            <div className="container">
+                                <h1 className='enriq jumbo-title p-3 m-auto text-center subtitle-overlay'>Zealand Pinnacle 2021</h1>
+                                <h3 className='enriq jumbo-title p-3 m-auto subtitle-overlay muted'>April 28 - May 2, 2021</h3>
+                            </div>
+                            <br></br>
+                            <Button as={HLink} to="resources" variant="light" size="lg" className="intro-button ibarra shadow">Resources</Button>
+                            <Button href="https://www.google.com" target="_blank" variant="light" size="lg" className="intro-button ibarra shadow">Register</Button>
+                        </Col>
+                        <Col className="d-none d-lg-block">
+                            {/* <img className="logo-img" src={logo} alt="logo"></img> */}
+                        </Col>
+                         {/* mobile */}
+                         <Col className="d-lg-none">
+                            {/* <img className="logo-img center" src={logo} alt="logo"></img> */}
+                        </Col>
+                         <Col className='m-auto text-center intro-container d-lg-none'>
+                             <div className="container">
+                            <h1 className='ibarra jumbo-title p-3 m-auto text-center subtitle-overlay'>Zealand Pinnacle 2021</h1>
+                            </div>
+                            <br></br>
+                            <Button as={HLink} to="resources" variant="light" className="intro-button ibarra-small shadow">Agenda</Button>
+                            <Button as={HLink} to="registration" variant="light" className="intro-button ibarra-small shadow">Schedule</Button>
+                        </Col>
+                        
+                    </Row>
+                </Jumbotron>
+            </Row>
+        </Container>
         </>
     )
 }
