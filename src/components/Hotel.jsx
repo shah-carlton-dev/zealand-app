a923c850396d54cdf423e4e008e5c2970d1041e5
maccarlton25 <39240058+maccarlton25@users.noreply.github.com>
2021-01-13 12:15:32 -0500

    added hotel structure

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
-
+import { Container, Button, Row, Col, Image } from "react-bootstrap";
+import hotel from "../assets/hotel.webp";
 const Hotel = () => {
 
     return (
@@ -8,6 +8,22 @@ const Hotel = () => {
             <Container id="hotelsec">
                 <Row>
                     <h2> Hotel Section </h2>
+                </Row>
+                <Row>
+                    <div className="col-md-6">
+                        <Image rounded src="https://images.rosewoodhotels.com/is/image/rwhg/rosewoodlittledixbay-aerialview:WIDE-LARGE-16-9" fluid></Image>
+                    </div>
+                    <div className="col-md-6">
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in ex libero. 
+                            Quisque ornare quam non leo cursus, a molestie sem sollicitudin. Nam ullamcorper 
+                            accumsan nibh non eleifend. Maecenas ut felis a ligula malesuada consectetur. 
+                            Phasellus vitae odio eu lectus lobortis consequat consectetur ac erat. 
+                        </p>
+                        <div className="text-center">
+                            <Button variant="outline-secondary" href="https://www.rosewoodhotels.com/en/little-dix-bay-virgin-gorda" target="_blank">Visit the hotel site</Button>{' '}
+                        </div>
+                    </div>
                 </Row>
             </Container>
         </>
