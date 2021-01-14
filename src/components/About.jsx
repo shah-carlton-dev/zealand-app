9dafb70dafad07c901b7dc366ac2da201101f68a
rushil-shah87 <54685822+rushil-shah87@users.noreply.github.com>
2021-01-14 16:44:26 -0500

    fix carousel and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                     <Col md={6}>
                         <Image src="https://images.rosewoodhotels.com/is/image/rwhg/rosewoodlittledixbay-aerialview:WIDE-LARGE-16-9" fluid />
                     </Col>
-                    <Col md={6} className="px-5 align-items-center hotel-text">
+                    <Col md={{span: 6, order: 1}} className="px-5 align-items-center hotel-text">
                         <div>
                             <h3 className="align-items-center nun">ROSEWOOD LITTLE DIX BAY </h3>
                             <p className="text-center"> Long regarded as one of the world’s greatest getaways, Rosewood Little Dix Bay offers a level of refinement, seclusion, and untamed beauty unrivaled in the Caribbean.</p>
@@ -22,7 +22,7 @@ const About = () => {
                             </div>
                         </div>
                     </Col>
-                    <Col md={6} xs={{span: 12, order: 3}}className="hotel-text align-items-center px-5">
+                    <Col md={{span: 6, order: 2}} xs={{span: 12, order: 3}}className="hotel-text align-items-center px-5">
                         <div class=" ">
                             <h3 className="align-items-center nun">ABOUT POINT 2</h3>
                             <p className="text-center"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in ex libero. Quisque ornare quam non leo cursus, a molestie sem sollicitudin. Nam ullamcorper accumsan nibh non eleifend</p>
@@ -31,13 +31,13 @@ const About = () => {
                             </div>
                         </div>
                     </Col>
-                    <Col md={6} xs={{span: 12, order: 2}}>
+                    <Col md={{span: 6, order: 3}} xs={{span: 12, order: 2}}>
                         <Image src="https://images.rosewoodhotels.com/is/image/rwhg/rosewoodlittledixbay-pool:WIDE-LARGE-16-9" fluid />
                     </Col>
-                    <Col md={6} xs={{span: 12, order: 4}}>
+                    <Col md={{span: 6, order: 4}} xs={{span: 12, order: 4}}>
                         <Image src="https://images.rosewoodhotels.com/is/image/rwhg/rosewoodlittledixbay-pavilionexterior-1-1:WIDE-LARGE-16-9" fluid />
                     </Col>
-                    <Col md={6} xs={{span: 12, order: 5}}className="px-5 align-items-center hotel-text">
+                    <Col md={{span: 6, order: 5}} xs={{span: 12, order: 5}}className="px-5 align-items-center hotel-text">
                         <div>
                             <h3 className="align-items-center nun">ABOUT POINT 3 </h3>
                             <p className="text-center"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in ex libero. Quisque ornare quam non leo cursus, a molestie sem sollicitudin. Nam ullamcorper accumsan nibh non eleifend </p>
